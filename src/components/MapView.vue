e7feb7b3be24ad82a67e37121e31e147489ac3ef
Thomas Calmant <thomas.calmant@gmail.com>
2025-03-29 18:54:41 +0100

    Added OpenTopMap as base layer

--- src/components/MapView.vue
+++ src/components/MapView.vue
@@ -95,6 +95,11 @@ const initMap = () => {
       maxZoom: 19,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     }),
+    OpenTopoMap: L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+      maxZoom: 15,
+      attribution:
+        'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
+    }),
   }
 
   L.control.layers(baseLayers, {}).addTo(map)
